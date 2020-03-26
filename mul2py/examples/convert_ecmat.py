cd907b6557cbbd520d128d8117d2e2f9bf867c45
Emil Christiansen <emil.christiansen@ntnu.no>
2020-03-26 10:46:55 +0100

    Make conversion script for command line

---
+++ mul2py/examples/convert_ecmat.py
@@ -0,0 +1,33 @@
+from mul2py import buildtools
+import argparse
+from pathlib import Path
+
+if __name__ == '__main__':
+    # Parser arguments
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('file_path', help='Filepath to the result file')
+
+    parser.add_argument('--overwrite', nargs='?', default=True,
+                        help='Overwrite existing output files? Default is True')
+
+    # Parse arguments
+    arguments = parser.parse_args()
+    filepath = Path(arguments.file_path)
+    sim_overwrite = bool(arguments.overwrite)
+
+    #Print arguments
+    args = vars(arguments)
+    print('\nGot {} arguments:'.format(len(args)))
+    for arg in args:
+        print('\t{} = {}'.format(arg, args[arg]))
+    print('\n')
+
+    #Check file type
+    if not filepath.suffix == '.ecmat':
+        raise ValueError('Can only load "ecmat" files, got "{}"'.format(filepath.suffix))
+
+    #Load and make signal
+    signal = buildtools.make_signal(filepath)
+
+    #Save signal as hyperspy file
+    signal.save(filepath.with_suffix('.hspy'), overwrite=sim_overwrite)
